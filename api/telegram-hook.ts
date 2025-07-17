6f4738a2885ae77575af58c969fe05eabea0e3aa
emerur <juiceless.9@gmail.com>
2025-07-17 01:40:24 +0000

    Update telegram-hook.ts

--- api/telegram-hook.ts
+++ api/telegram-hook.ts
@@ -12,7 +12,7 @@ const bot = new Telegraf(BOT_TOKEN);
 export async function handleStartCommand(ctx) {
   const COMMAND = "/start";
   const channelUrl = "t.me/nitrovpns0";
-  const targetUrl = "t.me/+KQx30rCFOeQ5Mzlk";
+  const targetUrl = "t.me/+GdntFEXNhM05ODRk";
 
   // Welcome message with Markdown formatting
   const reply = `
